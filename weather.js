3cdc13c3665a21720fc5b24f830da18d9dbdf975
Odera5 <anikingsleyodera5@gmail.com>
2024-12-15 22:53:22 +0100

    second commit

--- weather.js
+++ weather.js
@@ -32,8 +32,11 @@ btn.textContent = "submit";
 body.appendChild(btn);
 
 //fuction that takes location and print result
-function printResult(location) {
+
+function printResult() {
+  let location;
   location = input.value;
+  input.value = "";
   const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}%2C%20nigeria?unitGroup=us&key=X9LXFCATKQHBHUNZ7YEFW3EAW&contentType=json`;
   fetch(url, { mode: "cors" })
     .then((response) => {
@@ -43,7 +46,14 @@ function printResult(location) {
       const para = document.createElement("p");
       body.appendChild(para);
       result = data.currentConditions.humidity;
+      const btn1 = document.createElement("button");
       para.textContent = `${location} has humidity of ${result}`;
+      btn1.textContent = "Delete";
+      btn1.style.marginLeft = "10px";
+      para.appendChild(btn1);
+      btn1.addEventListener("click", () => {
+        body.removeChild(para);
+      });
     });
 }
 btn.addEventListener("click", printResult);
